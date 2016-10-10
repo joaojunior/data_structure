c9e382765886f44b785a39caafb30ac8ab678e7c
Joao Junior <jcajcefet@yahoo.com.br>
2016-10-10 09:11:50 -0300

    Add test to verify insertion nodes in graph

--- src/python/models.py
+++ src/python/models.py
@@ -1,19 +1,32 @@
 class Node(object):
-    def __init__(self):
-        self.label = ''
-        self.weight = 0
+    def __init__(self, label='', weight=0):
+        self.label = label
+        self.weight = weight
+
+
+class Edge(object):
+    def __init__(self, node_source, node_destination, label='', weight=0):
+        self.node_cource = node_source
+        self.node_dest = node_destination
+        self.label = label
+        self.weight = weight
 
 
 class Graph(object):
     def __init__(self):
-        self.nodes = {}
+        self.nodes = []
         self.edges = {}
 
     def number_of_nodes(self):
-        return len(self.nodes.keys())
+        return len(self.nodes)
 
     def number_of_edges(self):
         return len(self.edges.keys())
+
+    def add_node(self, label, weight=0):
+        node = Node(label, weight)
+        self.nodes.append(node)
+        return node
 
 
 class DirectedGraph(Graph):
--- src/python/tests/tests_models.py
+++ src/python/tests/tests_models.py
@@ -10,6 +10,12 @@ class BaseTestGraph(object):
     def test_number_of_edges_in_new_graph(self):
         self.assertEqual(0, self.graph.number_of_edges())
 
+    def test_add_node(self):
+        actual = self.graph.add_node(label='node1', weight=1)
+        self.assertEqual(1, self.graph.number_of_nodes())
+        self.assertEqual('node1', actual.label)
+        self.assertEqual(1, actual.weight)
+
 
 class TestDirectedGraph(unittest.TestCase, BaseTestGraph):
     def setUp(self):
