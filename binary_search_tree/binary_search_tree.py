62fcd0f8ce05cf6d8b0a5a1fc32af2a571cf0155
Joao Junior <jcajcefet@yahoo.com.br>
2018-02-18 14:07:49 -0300

    Add method insert and tests

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -2,9 +2,36 @@ class Node():
     def __init__(self, id_, value):
         self.id_ = id_
         self.value = value
+        self.left = None
+        self.right = None
+
+    def __eq__(self, other):
+        return self.id_ == other.id_ and self.value == other.value
 
 
 class BinarySearchTree():
     def __init__(self):
         self.number_of_nodes = 0
         self.root = None
+
+    def insert(self, node):
+        node = Node(node.id_, node.value)
+        if self.root is None:
+            self.root = node
+        else:
+            root = self.root
+            inserted = False
+            while inserted is False:
+                if node.value >= root.value:
+                    if root.right is None:
+                        root.right = node
+                        inserted = True
+                    else:
+                        root = root.right
+                else:
+                    if root.left is None:
+                        root.left = node
+                        inserted = True
+                    else:
+                        root = root.left
+        self.number_of_nodes += 1
--- binary_search_tree/binary_search_tree_tests.py
+++ binary_search_tree/binary_search_tree_tests.py
@@ -17,30 +17,93 @@ class TestBinarySearchTree(unittest.TestCase):
         self.assertEqual(None, self.bst.root)
 
     def test_insert_one_node(self):
-        pass
+        node1 = create_node(1, 1)
+
+        self.bst.insert(node1)
+        self.assertEqual(1, self.bst.number_of_nodes)
+        self.assertEqual(node1, self.bst.root)
 
     def test_insert_two_nodes_in_ascendent_order(self):
-        pass
+        node1 = create_node(1, 1)
+        node2 = create_node(2, 2)
+
+        self.bst.insert(node1)
+        self.bst.insert(node2)
+        self.assertEqual(2, self.bst.number_of_nodes)
+        self.assertEqual(node1, self.bst.root)
+        self.assertEqual(None, self.bst.root.left)
+        self.assertEqual(node2, self.bst.root.right)
 
     def test_insert_two_nodes_in_descendent_order(self):
-        pass
+        node1 = create_node(1, 1)
+        node2 = create_node(2, 2)
+
+        self.bst.insert(node2)
+        self.bst.insert(node1)
+        self.assertEqual(2, self.bst.number_of_nodes)
+        self.assertEqual(node2, self.bst.root)
+        self.assertEqual(node1, self.bst.root.left)
+        self.assertEqual(None, self.bst.root.right)
 
     def test_insert_tree_nodes_in_ascendent_order(self):
-        pass
+        node1 = create_node(1, 1)
+        node2 = create_node(2, 2)
+        node3 = create_node(3, 3)
+
+        self.bst.insert(node1)
+        self.bst.insert(node2)
+        self.bst.insert(node3)
+        self.assertEqual(3, self.bst.number_of_nodes)
+        self.assertEqual(node1, self.bst.root)
+        self.assertEqual(None, self.bst.root.left)
+        self.assertEqual(node2, self.bst.root.right)
+        self.assertEqual(node3, self.bst.root.right.right)
 
     def test_insert_tree_nodes_in_descendent_order(self):
-        pass
+        node1 = create_node(1, 1)
+        node2 = create_node(2, 2)
+        node3 = create_node(3, 3)
+
+        self.bst.insert(node3)
+        self.bst.insert(node2)
+        self.bst.insert(node1)
+        self.assertEqual(3, self.bst.number_of_nodes)
+        self.assertEqual(node3, self.bst.root)
+        self.assertEqual(None, self.bst.root.right)
+        self.assertEqual(node2, self.bst.root.left)
+        self.assertEqual(node1, self.bst.root.left.left)
 
     def test_insert_tree_nodes(self):
-        pass
+        node1 = create_node(1, 1)
+        node2 = create_node(2, 2)
+        node3 = create_node(3, 3)
+
+        self.bst.insert(node2)
+        self.bst.insert(node3)
+        self.bst.insert(node1)
+        self.assertEqual(3, self.bst.number_of_nodes)
+        self.assertEqual(node2, self.bst.root)
+        self.assertEqual(node3, self.bst.root.right)
+        self.assertEqual(node1, self.bst.root.left)
+
+    def test_insert_tree_equal_nodes(self):
+        node1 = create_node(1, 1)
+
+        self.bst.insert(node1)
+        self.bst.insert(node1)
+        self.bst.insert(node1)
+        self.assertEqual(3, self.bst.number_of_nodes)
+        self.assertEqual(node1, self.bst.root)
+        self.assertEqual(node1, self.bst.root.right)
+        self.assertEqual(node1, self.bst.root.right.right)
 
     def test_search_root_node(self):
         pass
 
-    def test_right_leaf(Self):
+    def test_search_right_leaf(Self):
         pass
 
-    def test_left_leaf(self):
+    def test_search_left_leaf(self):
         pass
 
     def test_remove_root_with_no_children(self):
