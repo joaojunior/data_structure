04476f897e19e49bcbfae5859fc249f23cb29821
Joao Junior <jcajcefet@yahoo.com.br>
2018-02-18 11:36:04 -0300

    Add remove method and the tests

--- binary_tree/binary_tree_tests.py
+++ binary_tree/binary_tree_tests.py
@@ -93,26 +93,135 @@ class TestBinaryTree(unittest.TestCase):
         result = self.binary_tree.search(4)
         self.assertEqual(None, result)
 
-    def test_search_element_not_exist(self):
-        pass
-
     def test_remove_head_with_no_children(self):
-        pass
+        item1 = create_item(1, 1)
+
+        self.binary_tree.insert(item1)
+
+        result = self.binary_tree.remove(item1.id_)
+        self.assertEqual(1, result.id_)
+        self.assertEqual(1, result.value)
+        self.assertEqual(0, self.binary_tree.size)
+        self.assertEqual(None, self.binary_tree.root)
 
     def test_remove_head_with_one_child(self):
-        pass
+        item1 = create_item(1, 1)
+        item2 = create_item(2, 2)
+
+        self.binary_tree.insert(item1)
+        self.binary_tree.insert(item2)
+
+        result = self.binary_tree.remove(item1.id_)
+        self.assertEqual(1, result.id_)
+        self.assertEqual(1, result.value)
+        self.assertEqual(1, self.binary_tree.size)
+        self.assertEqual(2, self.binary_tree.root.id_)
+        self.assertEqual(2, self.binary_tree.root.value)
+        self.assertEqual(None, self.binary_tree.root.left)
+        self.assertEqual(None, self.binary_tree.root.right)
 
     def test_remove_head_with_two_children(self):
-        pass
+        item1 = create_item(1, 1)
+        item2 = create_item(2, 2)
+        item3 = create_item(3, 3)
+
+        self.binary_tree.insert(item1)
+        self.binary_tree.insert(item2)
+        self.binary_tree.insert(item3)
+
+        result = self.binary_tree.remove(item1.id_)
+        self.assertEqual(1, result.id_)
+        self.assertEqual(1, result.value)
+        self.assertEqual(2, self.binary_tree.size)
+        self.assertEqual(3, self.binary_tree.root.id_)
+        self.assertEqual(3, self.binary_tree.root.value)
+        self.assertEqual(2, self.binary_tree.root.left.id_)
+        self.assertEqual(2, self.binary_tree.root.left.value)
+        self.assertEqual(None, self.binary_tree.root.right)
 
     def test_remove_leaf(self):
-        pass
+        item1 = create_item(1, 1)
+        item2 = create_item(2, 2)
+        item3 = create_item(3, 3)
+        item4 = create_item(4, 4)
+
+        self.binary_tree.insert(item1)
+        self.binary_tree.insert(item2)
+        self.binary_tree.insert(item3)
+        self.binary_tree.insert(item4)
+
+        result = self.binary_tree.remove(item4.id_)
+        self.assertEqual(item4, result)
+        self.assertEqual(3, self.binary_tree.size)
+        self.assertEqual(item1, self.binary_tree.root)
+        self.assertEqual(item2, self.binary_tree.root.left)
+        self.assertEqual(item3, self.binary_tree.root.right)
 
     def test_remove_middle_with_one_child(self):
-        pass
+        item1 = create_item(1, 1)
+        item2 = create_item(2, 2)
+        item3 = create_item(3, 3)
+        item4 = create_item(4, 4)
+
+        self.binary_tree.insert(item1)
+        self.binary_tree.insert(item2)
+        self.binary_tree.insert(item3)
+        self.binary_tree.insert(item4)
+
+        result = self.binary_tree.remove(item2.id_)
+        self.assertEqual(2, result.id_)
+        self.assertEqual(2, result.value)
+        self.assertEqual(2, result.value)
+        self.assertEqual(3, self.binary_tree.size)
+        self.assertEqual(item1, self.binary_tree.root)
+        self.assertEqual(3, self.binary_tree.root.left.id_)
+        self.assertEqual(3, self.binary_tree.root.left.value)
+        self.assertEqual(item4, self.binary_tree.root.left.left)
 
     def test_remove_middle_with_two_children(self):
-        pass
+        item1 = create_item(1, 1)
+        item2 = create_item(2, 2)
+        item3 = create_item(3, 3)
+        item4 = create_item(4, 4)
+        item5 = create_item(5, 5)
+
+        self.binary_tree.insert(item1)
+        self.binary_tree.insert(item2)
+        self.binary_tree.insert(item3)
+        self.binary_tree.insert(item4)
+        self.binary_tree.insert(item5)
+
+        result = self.binary_tree.remove(item2.id_)
+        self.assertEqual(2, result.id_)
+        self.assertEqual(2, result.value)
+        self.assertEqual(4, self.binary_tree.size)
+        self.assertEqual(item1, self.binary_tree.root)
+        self.assertEqual(3, self.binary_tree.root.left.id_)
+        self.assertEqual(3, self.binary_tree.root.left.value)
+        self.assertEqual(item4, self.binary_tree.root.left.left)
+        self.assertEqual(item5, self.binary_tree.root.left.right)
+
+    def test_remove_item_not_exist(self):
+        item1 = create_item(1, 1)
+        item2 = create_item(2, 2)
+        item3 = create_item(3, 3)
+        item4 = create_item(4, 4)
+        item5 = create_item(5, 5)
+
+        self.binary_tree.insert(item1)
+        self.binary_tree.insert(item2)
+        self.binary_tree.insert(item3)
+        self.binary_tree.insert(item4)
+        self.binary_tree.insert(item5)
+
+        result = self.binary_tree.remove(6)
+        self.assertEqual(None, result)
+        self.assertEqual(5, self.binary_tree.size)
+        self.assertEqual(item1, self.binary_tree.root)
+        self.assertEqual(item2, self.binary_tree.root.left)
+        self.assertEqual(item3, self.binary_tree.root.right)
+        self.assertEqual(item4, self.binary_tree.root.left.left)
+        self.assertEqual(item5, self.binary_tree.root.left.right)
 
 
 if __name__ == '__main__':
